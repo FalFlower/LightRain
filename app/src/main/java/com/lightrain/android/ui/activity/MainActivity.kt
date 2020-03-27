88c3290020f3fc235073b31637a0a28d8778bbeb
FalFlower <zt847269989@outlook.com>
2020-03-27 13:25:29 +0800

    clean 开始写用户登录界面等

--- app/src/main/java/com/lightrain/android/ui/activity/MainActivity.kt
+++ app/src/main/java/com/lightrain/android/ui/activity/MainActivity.kt
@@ -16,77 +16,12 @@ import okhttp3.FormBody
 import okhttp3.RequestBody
 
 
-class MainActivity : AppCompatActivity(), ResponseHandler {
-    val username="15244482896"
-    val password="123456"
-    val userInfoBean=getUserInfo()
-    val formBody=FormBody.Builder()
-        .add("username",userInfoBean.username)
-        .add("password",userInfoBean.password)
-        .add("nickName",userInfoBean.nickname)
-        .add("userIcon",userInfoBean.userIcon)
-        .add("userGender",userInfoBean.userGender.toString())
-        .add("userBirthday",userInfoBean.userBirthday)
-        .add("userSchool",userInfoBean.userSchool)
-        .build()
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        login.setOnClickListener{
-            HttpManager.manager.
-            sendRequestByGet(URLProviderUtils.getUserInfoLoginUrl(username,password),
-                ResponseStatus.LOGIN,this)
 
-        }
-        logout.setOnClickListener{
-            HttpManager.manager.
-            sendRequestByGet(URLProviderUtils.getUserInfoLogoutUrl(username),
-                ResponseStatus.LOGIN,this)
-
-        }
-        update.setOnClickListener{
-            HttpManager.manager.
-            sendRequestByPost(URLProviderUtils.getUserInfoUpdate(),formBody,
-                ResponseStatus.LOGIN,this)
-
-        }
-        register.setOnClickListener{
-            HttpManager.manager.
-            sendRequestByPost(URLProviderUtils.getUserInfoRegister(),formBody,
-                ResponseStatus.LOGIN,this)
-
-        }
-    }
-
-    override fun onError(msg: String?) {
-        println("MainActivity onError: $msg")
-        val exception= msg?.let { JsonUtil.getResponseException(it) }
-        exception?.let {
-            println("onError javaClass: ${exception.javaClass}")
-            println("onError status: ${exception.status}")
-            println("onError timestamp: ${exception.timestamp}")
-            println("onError error: ${exception.error}")
-            println("onError message: ${exception.message}")
-            println("onError path: ${exception.path}")
-
-        }
-    }
-
-    override fun onSuccess(status: ResponseStatus, result: String?) {
-        val data= result?.let { JsonUtil.getResultResponse(it,UserInfoBean::class.java)}
-        data?.let {
-            println("onSuccess javaClass: ${data.javaClass}")
-            println("onSuccess code: ${data.code}")
-            println("onSuccess msg: ${data.msg}")
-            println("onSuccess data: ${data.data}")
-        }
-
-    }
-
-    private fun getUserInfo():UserInfoBean{
-        return UserInfoBean("13562663180","13562663180","dad",
-            "http://zt-data.test.upcdn.net/icon.jpg",1,"1976-1-1",
-            44,"StayHome",0)
     }
 
 
